61c47f276c1a2380d6f163d0f65631fae00e2f6c
Val Delchev <btwonu@gmail.com>
2021-03-25 13:24:56 +0200

    Add state functionality to forms

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Component } from 'react';
 
 // Bootstrap
 import Form from 'react-bootstrap/Form';
@@ -7,24 +7,56 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
-const LoginForm = () => {
-  return (
-    <Form className="border p-4 mt-4">
-      <Form.Group id="login-email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-      <Form.Group id="login-password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter password" />
-      </Form.Group>
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
-      <Button variant="primary" type="submit" block>
-        Login
-      </Button>
-    </Form>
-  );
-};
+  handleChange = (e) => {
+    let { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit} className="border p-4 mt-4">
+        <Form.Group id="login-email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group id="login-password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" block>
+          Login
+        </Button>
+      </Form>
+    );
+  }
+}
 
 export default LoginForm;
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Component } from 'react';
 
 // Bootstrap
 import Form from 'react-bootstrap/Form';
@@ -7,36 +7,77 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
-const RegisterForm = () => {
-  return (
-    <Form className="border p-4 mt-4">
-      <Form.Group className="pt-2" as={Row}>
-        <Form.Label column xs={6} sm={4} md={6}>
-          Username
-        </Form.Label>
-        <Col xs={6} sm={8} md={6}>
-          <Form.Control readOnly defaultValue="User" />
-        </Col>
-      </Form.Group>
-      <Form.Group id="register-email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+class RegisterForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
 
-      <Form.Group id="register-password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter password" />
-      </Form.Group>
-      <Form.Group id="register-confirm">
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm password" />
-      </Form.Group>
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
-      <Button variant="primary" type="submit" block>
-        Register
-      </Button>
-    </Form>
-  );
-};
+  handleChange = (e) => {
+    let { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit} className="border p-4 mt-4">
+        <Form.Group className="pt-2" as={Row}>
+          <Form.Label column xs={6} sm={4} md={6}>
+            Username
+          </Form.Label>
+          <Col xs={6} sm={8} md={6}>
+            <Form.Control readOnly defaultValue="User" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group id="register-email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group id="register-password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group id="register-confirm">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            value={this.state.confirmPassword}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" block>
+          Register
+        </Button>
+      </Form>
+    );
+  }
+}
 
 export default RegisterForm;

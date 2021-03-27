c92f958a59dc7e014d1cf284cacad9ccf5ef81e8
Val Delchev <btwonu@gmail.com>
2021-03-27 10:03:16 +0200

    Add initial Details page UI and youtube service

--- src/pages/Movie/Details.js
+++ src/pages/Movie/Details.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import { Component } from 'react';
 
-const Details = ({ match, history, location }) => {
-  console.log(match, history, location);
-  return <h1>{match.params.movieId}</h1>;
-};
+// Services
+import movieService from '../../services/movieService';
+import youtubeService from '../../services/youtubeService';
+
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: {},
+    };
+  }
+
+  componentDidMount() {
+    let { movieId } = this.props.match.params;
+
+    movieService.getOne(movieId).then((movie) => {
+      this.setState({ movie });
+
+      youtubeService
+        .getTrailer(movie.title)
+        .then((trailer) => this.setState({ trailer }));
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h2 className="text-center">{this.state.movie.title}</h2>
+
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe
+            src={this.state.trailer}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Details;

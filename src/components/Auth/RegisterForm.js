ace529da2e10e64bcb18da6e5458042acad913c0
Val Delchev <btwonu@gmail.com>
2021-03-29 14:21:46 +0300

    Add initial register logic

--- src/components/Auth/RegisterForm.js
+++ src/components/Auth/RegisterForm.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import axios from 'axios';
 
 // Bootstrap
 import Form from 'react-bootstrap/Form';
@@ -11,6 +12,7 @@ class RegisterForm extends Component {
   constructor() {
     super();
     this.state = {
+      username: '',
       email: '',
       password: '',
       confirmPassword: '',
@@ -19,7 +21,32 @@ class RegisterForm extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+
+    let { username, email, password, confirmPassword } = this.state;
+
+    axios({
+      method: 'post',
+      url:
+        'http://localhost:5001/movie-find-dev/europe-west1/api/auth/register',
+      data: {
+        username,
+        email,
+        password,
+        confirmPassword,
+      },
+    })
+      .then((res) => {
+        localStorage.setItem('AuthToken', `Bearer ${res.data.JWT}`);
+        console.log('Registration done!');
+      })
+      .catch((err) => {
+        console.log('Registration error:', err);
+        // handle error
+        // this.setState({
+        //   errors: err.response.data,
+        //   loading: false,
+        // });
+      });
   };
 
   handleChange = (e) => {
@@ -35,7 +62,13 @@ class RegisterForm extends Component {
             Username
           </Form.Label>
           <Col xs={6} sm={8} md={6}>
-            <Form.Control readOnly defaultValue="User" />
+            <Form.Control
+              type="text"
+              name="username"
+              placeholder="Your visible nickname"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </Col>
         </Form.Group>
 

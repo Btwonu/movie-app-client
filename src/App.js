3f3dba2e9e5543b04fe006332f546705addd1f61
Val Delchev <btwonu@gmail.com>
2021-04-10 20:47:19 +0300

    Add conditional default app link

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ import { AuthContext, useAuth } from './contexts/AuthContext';
 // Components
 import { AuthenticatedRoutes, UnauthenticatedRoutes } from './routes';
 
-axios.defaults.baseURL =
-  'http://localhost:5001/movie-find-dev/europe-west1/api';
+// axios.defaults.baseURL =
+//   'http://localhost:5001/movie-find-dev/europe-west1/api';
+
+if (process.env.NODE_ENV === 'development') {
+  console.log(process.env.NODE_ENV);
+  axios.defaults.baseURL =
+    'http://localhost:5001/movie-find-dev/europe-west1/api';
+} else if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL =
+    'https://europe-west1-movie-find-dev.cloudfunctions.net/api';
+}
 
 const App = () => {
   const { user } = useAuth();

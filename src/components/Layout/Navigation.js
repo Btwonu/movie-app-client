b146e727cc4e763a5cc49141457740fbc4848cb5
Val Delchev <btwonu@gmail.com>
2021-06-01 12:15:18 +0300

    wrapped navigation to align it better with rest of content

--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -8,60 +8,64 @@ import Nav from 'react-bootstrap/Nav';
 
 import styled from 'styled-components';
 
-const Styled = styled.div`
+const StyledDiv = styled.div`
   .navbar-light .navbar-nav .nav-link {
     color: #343434;
   }
 `;
 
+const NavigationWrapper = styled.div``;
+
 function Navigation() {
   const { user, logout, updateUser } = useAuth();
 
   return (
-    <Styled>
+    <StyledDiv>
       <Navbar
         className="d-flex justify-content-between"
         bg="primary"
         variant="light"
         fixed="top"
       >
-        <Navbar.Brand as={Link} to="/">
-          Movie<strong>Find</strong>
-        </Navbar.Brand>
+        <NavigationWrapper className="container">
+          <Navbar.Brand as={Link} to="/">
+            Movie<strong>Find</strong>
+          </Navbar.Brand>
 
-        <Nav>
-          {user ? (
-            <>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/collections">
-                Collections
-              </Nav.Link>
-              <Nav.Link as={Link} to="/users/user">
-                Profile
-              </Nav.Link>
-              <Nav.Link as={Link} onClick={logout}>
-                Logout
-              </Nav.Link>
-            </>
-          ) : (
-            <>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/auth/login">
-                Login
-              </Nav.Link>
+          <Nav>
+            {user ? (
+              <>
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/collections">
+                  Collections
+                </Nav.Link>
+                <Nav.Link as={Link} to="/users/user">
+                  Profile
+                </Nav.Link>
+                <Nav.Link as={Link} onClick={logout}>
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/auth/login">
+                  Login
+                </Nav.Link>
 
-              <Nav.Link as={Link} to="/auth/register">
-                Sign Up
-              </Nav.Link>
-            </>
-          )}
-        </Nav>
+                <Nav.Link as={Link} to="/auth/register">
+                  Sign Up
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </NavigationWrapper>
       </Navbar>
-    </Styled>
+    </StyledDiv>
   );
 }
 

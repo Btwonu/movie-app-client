a4fd0761370c8a5031672fface9299473c366a1e
Val Delchev <btwonu@gmail.com>
2023-03-12 15:23:45 +0200

    Convert RegisterForm into a functional component; Address register issues

--- src/components/Auth/RegisterForm.js
+++ src/components/Auth/RegisterForm.js
@@ -1,6 +1,8 @@
-import { Component } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 
+import axios from 'axios';
+import { useAuth } from '../../contexts/AuthContext';
 import { validateSignupData } from '../../utils/validators';
 
 // Bootstrap
@@ -9,135 +11,121 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-class RegisterForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      errors: {},
-    };
-  }
+const RegisterForm = ({history}) => {
+	const [formData, setFormData] = useState({
+		username: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
+	const [errors, setErrors] = useState({});
+	const { updateUser } = useAuth();
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({ errors: {} });
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value
+		});
+	};
 
-    let { username, email, password, confirmPassword } = this.state;
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErrors({ errors: {} });
 
-    let { errors, valid } = validateSignupData(
-      username,
-      email,
-      password,
-      confirmPassword
-    );
+		const { errors, valid } = validateSignupData(
+			formData.username,
+			formData.email,
+			formData.password,
+			formData.confirmPassword
+		);
 
-    if (!valid) {
-      this.setState({ errors });
-      return;
-    }
+		if (!valid) {
+			setErrors({ errors });
+			return;
+		}
 
-    axios({
-      method: 'post',
-      url: 'http://localhost:5001/movie-find-dev/europe-west1/api/auth/register',
-      data: {
-        username,
-        email,
-        password,
-        confirmPassword,
-      },
-    })
-      .then((res) => {
-        localStorage.setItem('AuthToken', `Bearer ${res.data.JWT}`);
-        console.log('Registration done!');
-      })
-      .catch((err) => {
-        console.log('Registration error:', err);
-        // handle error
-        // this.setState({
-        //   errors: err.response.data,
-        //   loading: false,
-        // });
-      });
-  };
+		axios({
+			method: 'post',
+			url: '/auth/register',
+			data: formData,
+		  })
+			.then((res) => {
+			  localStorage.setItem('AuthToken', `Bearer ${res.data.JWT}`);
+			  history.push('/');
+			  updateUser();
+			})
+			.catch((err) => {
+			  console.log('Registration error:', err);
+			});
+	};
 
-  handleChange = (e) => {
-    let { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
+	return (
+		<Form onSubmit={handleSubmit} className="border shadow-sm p-4 mt-4">
+			<Form.Group className="pt-2" as={Row}>
+			<Form.Label column xs={6} sm={4} md={6}>
+				Username
+			</Form.Label>
+			<Col xs={6} sm={8} md={6}>
+				<Form.Control
+				type="text"
+				name="username"
+				placeholder="Your visible nickname"
+				value={formData.username}
+				onChange={handleChange}
+				/>
+				<div className="error">
+					{errors.username && errors.username}
+				</div>
+			</Col>
+			</Form.Group>
 
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit} className="border shadow-sm p-4 mt-4">
-        <Form.Group className="pt-2" as={Row}>
-          <Form.Label column xs={6} sm={4} md={6}>
-            Username
-          </Form.Label>
-          <Col xs={6} sm={8} md={6}>
-            <Form.Control
-              type="text"
-              name="username"
-              placeholder="Your visible nickname"
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-            <div className="error">
-              {this.state.errors.username && this.state.errors.username}
-            </div>
-          </Col>
-        </Form.Group>
+			<Form.Group id="register-email">
+			<Form.Label>Email</Form.Label>
+			<Form.Control
+				type="email"
+				name="email"
+				placeholder="Enter email"
+				value={formData.email}
+				onChange={handleChange}
+			/>
+			<div className="error">
+				{errors.email && errors.email}
+			</div>
+			</Form.Group>
 
-        <Form.Group id="register-email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <div className="error">
-            {this.state.errors.email && this.state.errors.email}
-          </div>
-        </Form.Group>
+			<Form.Group id="register-password">
+			<Form.Label>Password</Form.Label>
+			<Form.Control
+				type="password"
+				name="password"
+				placeholder="Enter password"
+				value={formData.password}
+				onChange={handleChange}
+			/>
+			<div className="error">
+				{errors.password && errors.password}
+			</div>
+			</Form.Group>
 
-        <Form.Group id="register-password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Enter password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <div className="error">
-            {this.state.errors.password && this.state.errors.password}
-          </div>
-        </Form.Group>
+			<Form.Group id="register-confirm">
+			<Form.Label>Confirm Password</Form.Label>
+			<Form.Control
+				type="password"
+				name="confirmPassword"
+				placeholder="Confirm password"
+				value={formData.confirmPassword}
+				onChange={handleChange}
+			/>
+			<div className="error">
+				{errors.confirmPassword && errors.confirmPassword}
+			</div>
+			</Form.Group>
 
-        <Form.Group id="register-confirm">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm password"
-            value={this.state.confirmPassword}
-            onChange={this.handleChange}
-          />
-          <div className="error">
-            {this.state.errors.confirmPassword &&
-              this.state.errors.confirmPassword}
-          </div>
-        </Form.Group>
+			<Button variant="primary" type="submit" block>
+			Register
+			</Button>
+		</Form>
+	);
+};
 
-        <Button variant="primary" type="submit" block>
-          Register
-        </Button>
-      </Form>
-    );
-  }
-}
-
-export default RegisterForm;
+export default withRouter(RegisterForm);
